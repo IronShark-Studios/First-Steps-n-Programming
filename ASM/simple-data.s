054c751b0996c7c0015875f09dee0b2821e64f31
xin <git@ironshark.org>
2024-12-27 15:19:03 -0500

    adds more basics problems

---
+++ ASM/simple-data.s
@@ -0,0 +1,28 @@
+.globl _start
+
+    .section .data
+
+first_value:
+    .quad 4
+
+second_value:
+    .quad 6
+
+final_value:
+    .quad 0
+
+
+    .section .text
+
+_start:
+
+    movq first_value, %rbx
+    movq second_value, %rcx
+
+    addq %rbx, %rcx
+
+    movq %rcx, final_value
+
+    movq final_value, %rdi
+    movq $60, %rax
+    syscall
